7b41d56efbb354dbe2d940231e96f8546a996b25
Shunichi <shunichi.sd@gmail.com>
2018-05-13 20:25:53 +0900

    try to use some methods of canvas class

--- src/application/MakeController.java
+++ src/application/MakeController.java
@@ -4,12 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 
 public class MakeController {
 	@FXML Canvas canvas;
 	GraphicsContext gc;
 	@FXML Button quitBtn;
+	@FXML TextArea log;
+	double drawAreaWidth ;
+	double drawAreaHeight;
 	@FXML 
 	private void quitAction() {
 		System.exit(0);
@@ -19,6 +23,10 @@ public class MakeController {
 	@FXML
 	private void execAction() {
 		gc = canvas.getGraphicsContext2D();
+		drawAreaWidth = canvas.getWidth();
+		drawAreaHeight = canvas.getHeight();
+		log.appendText("width= "+drawAreaWidth);
+		log.appendText("\nheight= "+drawAreaHeight);
 		draw();
 	}
 	private void draw() {
